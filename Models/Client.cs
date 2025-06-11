177e9cf33e752189dd58bff17c5bebb89a237173
SkyComic-hash <skygamechunnal@gmail.com>
2025-06-12 02:54:03 +0300

    Client.cs

---
+++ Models/Client.cs
@@ -0,0 +1,9 @@
+namespace TaxiComponents.Models
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string Name { get; set; }
+        public decimal TotalExpenses { get; set; }
+    }
+}
