3d941682b5e061b0e1434319014146987cb277f7
SkyComic-hash <skygamechunnal@gmail.com>
2025-06-12 02:55:23 +0300

    Update Vehicle.cs

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,1 +1,13 @@
+using System;
+
+namespace TaxiComponents.Models
+{
+    public class Vehicle
+    {
+        public string VehicleId { get; set; }
+        public string DriverName { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public DateTime? AvailableFrom { get; set; } = null;
+    }
+}
 
